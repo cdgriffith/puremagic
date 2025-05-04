7510e3746e6fe8466db532ed9e9abb07e1337cf5
Chris Griffith <chris@cdgriffith.com>
2025-05-04 16:23:35 -0500

    Fix testing line endings with git issues

--- test/test_scanners.py
+++ test/test_scanners.py
@@ -1,34 +1,38 @@
-import pytest
-
 import puremagic
 from test.common import (
-    RESOURCE_DIR,
-    IMAGE_DIR,
-    VIDEO_DIR,
-    AUDIO_DIR,
     OFFICE_DIR,
-    ARCHIVE_DIR,
-    MEDIA_DIR,
-    SYSTEM_DIR,
-    LOCAL_DIR,
 )
+
+sample_text = b"""Lorem ipsum dolor sit amet, consectetur adipiscing elit,{ending}
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{ending}
+{ending}
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.{ending}
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.{ending}
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.{ending}
+"""
 
 
 def test_text_scanner():
     # Test the text scanner with a sample text file
-    results = puremagic.magic_file(OFFICE_DIR / "text_lf.txt")
+    lr_file = OFFICE_DIR / "text_lf.txt"
+    lr_file.write_bytes(sample_text.replace(b"\n", b"").replace(b"{ending}", b"\n"))
+    results = puremagic.magic_file(lr_file)
     assert results[0].extension == ".txt"
     assert results[0].name == "ASCII text, with LF line terminators"
     assert results[0].mime_type == "text/plain"
     assert results[0].confidence == 0.9
 
-    results = puremagic.magic_file(OFFICE_DIR / "text_crlf.txt")
+    crlf_file = OFFICE_DIR / "text_crlf.txt"
+    crlf_file.write_bytes(sample_text.replace(b"\n", b"").replace(b"{ending}", b"\r\n"))
+    results = puremagic.magic_file(crlf_file)
     assert results[0].extension == ".txt"
     assert results[0].name == "ASCII text, with CRLF line terminators"
     assert results[0].mime_type == "text/plain"
     assert results[0].confidence == 0.9
 
-    results = puremagic.magic_file(OFFICE_DIR / "text_cr.txt")
+    cr_file = OFFICE_DIR / "text_cr.txt"
+    cr_file.write_bytes(sample_text.replace(b"\n", b"").replace(b"{ending}", b"\r"))
+    results = puremagic.magic_file(cr_file)
     assert results[0].extension == ".txt"
     assert results[0].name == "ASCII text, with CR line terminators"
     assert results[0].mime_type == "text/plain"
